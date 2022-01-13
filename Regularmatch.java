54043da88f9d17135fc76b1c3bbf7194ea78f931
Youssef Chouay <ychou031@uottawa.ca>
2022-01-12 20:10:21 -0500

    second commit

--- Regularmatch.java
+++ Regularmatch.java
@@ -2,76 +2,8 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Regularmatch {
-    public static boolean isMatch(String s, String p) {
-        String comp = "";
-        if(p.indexOf( '*' ) == -1 && p.indexOf( '.' ) == -1){
-            return s.equals(p);
-        }else{
-            for(int i = 0;i<p.length();i++){
-                if(p.charAt(i) != '*' && p.charAt(i) != '.'){
-                    comp = comp +p.charAt(i);
-                }else{
-                    if(p.charAt(i) == '.'){
-                        if(comp.length() != 0){
-                            String temp = "";
-                            for(int t = 0;t< comp.length();t++){
-                                temp = temp + s.charAt(t);
-                            }
-                            if((temp == comp) == false){
-                                return false;
-                            }else{
-                                temp = "";
-                                for(int j = comp.length()-1;j<s.length();j++){
-                                    temp = temp + s.charAt(j);
-                                }
-                                s = temp;
-                            }
-                        }
-                        String temp = "";
-                        for(int t = 1; t < s.length(); t++){
-                            temp = temp + s.charAt(t);
-                        }
-                        s = temp;
-                        comp = "";
-                        
-                    }else{
-                        if(comp.length() == 0){
-                            return true;
-                        }else{
-                            int oi = 1;
-                            while (oi == 1){
-                                if(s.indexOf(comp) != 0){
-                                    //if(s.charAt(0) != comp.charAt(0)){
-                                    //    return false;
-                                    //}else{
-                                        comp = "";
-                                        oi = 2;
-                                    //}
-                                }
-                                if(comp.length() == s.length()){
-                                    return true;
-                                }
-                                String temp = "";
-                                for(int y = comp.length();y<s.length();y++){
-                                    temp = temp + s.charAt(y);
-                                }
-                                s = temp;
-                                
-                                
-                            }
-                            
-                        }
-                    }
-                }
-            }
-            if(comp.length() != 0){
-                return (comp.equals(s));
-            }else{
-                return false;
-            }
-        }
-    }
-    public static boolean isMatch1(String s, String p){
+    
+    public static boolean isMatch(String s, String p){
         LinkedList<String> results = new LinkedList<>();
         String comp = "";
         boolean inside = false;
@@ -181,12 +113,12 @@ public class Regularmatch {
         System.out.print("Enter your expression: ");
         String p= sc.nextLine();
         while(!p.trim().equals("Q")){
-            if(isMatch1(s, p)){
+            if(isMatch(s, p)){
                 System.out.println("The expression matches the pattern !");
             }else{
                 System.out.println("The expression DOESN'T matches the pattern !");
             }
-            System.out.println("Enter your another expression (or 'Q' to quit ): ");
+            System.out.print("Enter your another expression (or 'Q' to quit ): ");
             p= sc.nextLine();
         }
         sc.close();
